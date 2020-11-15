01fd436c2ef92fce56db6fcf6ee0a902bc16e870
FRIDAY <densyaharsora@gmail.com>
2020-11-15 23:12:27 +0530

    Updated for heroku

--- Weather-app/web-servers/src/app.js
+++ Weather-app/web-servers/src/app.js
@@ -5,6 +5,7 @@ const forecast = require('../src/forecast')
 
 const app= express()
 
+const port=  process.env.PORT || 3000
 const publicDirectoryPath=path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
@@ -71,8 +72,8 @@ app.get('*',(req,res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' +port)
 })
 
 
